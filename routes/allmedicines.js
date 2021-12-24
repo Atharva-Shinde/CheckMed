84a3325b41e4ac6e5bdbb06a5eeb2ee19ceb006b
Atharva Shinde <atharvashinde179@gmail.com>
2021-12-24 23:49:46 +0530

    set: routes

--- routes/allmedicines.js
+++ routes/allmedicines.js
@@ -1,9 +1,26 @@
 const express = require('express')
 const router = express.Router()
+const Medicine = require('./../models/medicinesSchema.js')
 
+router.get('/', async(req, res)=> {
+    let medicines = await Medicine.find()
+    res.render('allmedicines', {medicines: medicines})
+})
 
-router.get('/', (req, res)=> {
-    res.render('allmedicines.ejs')
+router.post('/', async(req,res)=>{
+    let medicine = new Medicine({
+        name: req.body.name,
+        usedfor: req.body.usedfor,
+        category: req.body.category,
+    })
+    try{
+        medicine = await medicine.save()
+        res.redirect('/all-medicines')
+    }
+    catch(e){
+        console.log(e)
+        res.render('/newmedicine')
+    }
 })
 
 module.exports = router
